4e0509028356877062a83fe17ad7d140dbd90c5d
KacperMrochen <39525370+KacperMrochen@users.noreply.github.com>
2022-12-02 19:25:45 +0100

    Karts alignment

--- src/Components/Kart.tsx
+++ src/Components/Kart.tsx
@@ -1,31 +1,56 @@
 import React from "react";
+import "../Styles/Kart.css"
+import "../Styles/App.css"
 
 const Kart = () => {
-
     return (
         <div className="karts">
-            <h2>PRAGA RACING KARTS</h2>
+            <h2 className="karts__title">PRAGA RACING KARTS</h2>
             <div className="karts__carousel">
-                <div className="kart1">
-                    <div className="kart1__title">Baby</div>
-                    <div className="kart1__image"></div>
-                    <div className="kart1__desc">The kart that starts your career</div>
-                </div>
-                <div className="kart1">
-                    <div className="kart1__title">Mini</div>
-                    <div className="kart1__image"></div>
-                    <div className="kart1__desc">The top product for young drivers.</div>
-                </div>
-                <div className="kart1">
-                    <div className="kart1__title">Dark</div>
-                    <div className="kart1__image"></div>
-                    <div className="kart1__desc">Entry level kart with high performance.</div>
-                </div>
-                <div className="kart1">
-                    <div className="kart1__title">Dragon Evo 2 Invictus Tacho Evo</div>
-                    <div className="kart1__image"></div>
-                    <div className="kart1__desc">Best racing experience for senior, junior, shifter and DD2 categories.</div>
-                </div>
+                <a href="https://pragaglobal.com/racing-karts/baby/">
+                    <div className="kart_card">
+                        <div>
+                            <img className="kart_card__image" src={require("../Img/baby.png")} alt="baby" />
+                            <h3 className="kart_card__title">Baby</h3>
+                        </div>
+                        <div>
+                            <p className="kart_card__desc">The kart that starts your career</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://pragaglobal.com/racing-karts/mini/">
+                    <div className="kart_card">
+                        <div>
+                            <img className="kart_card__image" src={require("../Img/mini.png")} alt="Mini kart"/>
+                            <h3 className="kart_card__title">Mini</h3>
+                        </div>
+                        <div>
+                            <p className="kart_card__desc">The top product for young drivers.</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://pragaglobal.com/racing-karts/dark/">
+                    <div className="kart_card">
+                        <div>
+                            <img className="kart_card__image" src={require("../Img/dark.png")} alt="Dark kart"/>
+                            <h3 className="kart_card__title">Dark</h3>
+                        </div>
+                        <div>
+                            <p className="kart_card__desc">Entry level kart with high performance.</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://pragaglobal.com/racing-karts/maxi/">
+                    <div className="kart_card">
+                        <div>
+                            <img className="kart_card__image" src={require("../Img/dragon.png")} alt="Dragon kart"/>
+                            <h3 className="kart_card__title">Dragon Evo 2 Invictus Evo Tacho Evo</h3>
+                        </div>
+                        <div>
+                            <p className="kart_card__desc">Best racing experience for senior, junior, shifter and DD2 categories.</p>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     )
--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Video = () => {
     return (
         <div className="video">
-            <iframe src="https://www.youtube.com/watch?v=bhig5wNYQK8" title="video"></iframe>
+            
         </div>
     ) 
 }
