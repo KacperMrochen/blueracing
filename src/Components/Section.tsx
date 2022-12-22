6dfcf3f4c0331d060740f338fe9f4c8076ed4b28
KacperMrochen <39525370+KacperMrochen@users.noreply.github.com>
2022-12-22 12:13:42 +0100

    section component merge from js to ts

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -3,13 +3,17 @@ import { useRef } from 'react';
 //import { useInView } from 'framer-motion';
 import '../Styles/Section.css';
 
- const Section = ({ children }) => {
+  type Props = {
+    children: JSX.Element,
+  }
+
+ const Section = ({ children }: Props) => {
 
     //is element in view state:
     const [isInView, setIsInView] = useState(false);
     
     //refference to the element:
-    const ref = useRef();
+    const ref = useRef<HTMLDivElement | null>(null);
     
     //alternative:
     //const isInView = useInView(ref);
@@ -17,13 +21,15 @@ import '../Styles/Section.css';
     //changing isInView state based on element intersection:
     useEffect(()=> {
       
-      const callback = (entries) => 
+      const callback = (entries:IntersectionObserverEntry[]) => 
         entries.forEach((entry) => {
             setIsInView(entry.isIntersecting);
         })
 
       const observer = new IntersectionObserver(callback);
-      observer.observe(ref.current);
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
     },[])
 
     return (
